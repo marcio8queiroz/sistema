095f82172bbaacfe79f8092d7cddeee6c9326c67
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-03-05 22:33:53 -0400

    aplicando bootstrap

--- admin/funcionarios.php
+++ admin/funcionarios.php
@@ -1,1 +1,10 @@
-Funcionarios
+<?php $pagina = 'funcionarios'; ?>
+
+<div class="row botao-novo">
+	<div class="col-md-12">
+		
+		<a id="btn-novo" data-toggle="modal" data-target="#modal"></a>
+		<a href="index.php?acao=<?php echo $pagina ?>&funcao=novo"  type="button" class="btn btn-info">Novo Funcionário</a>
+
+	</div>
+</div>
